27adfd791f9b377b561c3cde30ef12e08ef8853c
isaxito <63028133+isaxito@users.noreply.github.com>
2020-05-02 22:14:31 -0300

    Add files via upload
    Diccionario para fibertel, teniendo en cuenta que usan el prefijo 014+DNI o 004+DNI, y en mi caso solo DNI. Ejecutar esto va a crear el diccionario con las 3 combinaciones, se puede hacer facil si sabes usar crunch pero no tenia ganas de aprender y quería hacerlo rapido.

---
+++ gen.c
@@ -0,0 +1,29 @@
+// Diccionario con 3 patrones, DNIs que empiezan en 15000000
+// y terminan en 450000000, todos con un 014 adelante, despues el mismo
+// rango con un 004, y finalmente los DNI solos
+//
+// cuando me instalaron el internet el técnico usó solo mi dni, pero tambien
+// se les pone 004 y 014 según el genero del cliente (lo ultimo lo leí en internet y lo ví en algunas redes de mis amigos)
+//
+// solo hice esto porque era mas rapido que aprender a usar crunch
+
+#include <stdio.h>
+int main ()
+{
+   FILE * fp;
+   unsigned int dni_ini = 15000000;
+   unsigned int dni_fin = 45000000;
+   unsigned int  i;
+   fp = fopen ("dic.txt","w");   
+   for(i = dni_ini; i < dni_fin;i++){
+       fprintf (fp,"014%ld\n",i);
+   }
+   for(i = dni_ini; i < dni_fin;i++){
+       fprintf(fp,"004%ld\n",i);
+   }
+   for(i = dni_ini; i < dni_fin;i++){
+       fprintf(fp,"%ld\n",i);
+   }
+   fclose (fp);
+   return 0;
+}
